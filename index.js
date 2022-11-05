5c027fcdc1411651974ec40f86d1c7b526ec5960
Ankit Maurya <am8365965@gmail.com>
2022-11-05 11:32:15 +0530

    added dollar exchange on index.html

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-var a = document.getElementById("btn1").style.color="blue";
+
+// API fetch function for getting the current dollar exchange data
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "PzafMwBIDeT9gKZ5ZIvbw50Teq1662rG");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch(`https://api.apilayer.com/fixer/convert?to=INR&from=USD&amount=1`, requestOptions).then((data)=>{
+    // console.log(data);
+    return data.json(); 
+}).then((requestOptions)=>{
+    console.log(requestOptions);
+    document.getElementById("r1").innerHTML=requestOptions?.date;
+    document.getElementById("r6").innerHTML=requestOptions?.info?.rate;
+});
